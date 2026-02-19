e94970d0c472cedcaf80676aaf3fe9cb9ce7bd81
duytq <tranduy216@gmail.com>
2026-02-19 20:48:13 +0700

    Deploy testing

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://tranduy216.github.io',
+    base: '/my-blog',
+});
